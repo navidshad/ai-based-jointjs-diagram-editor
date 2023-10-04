a42cdfb4545cf0bd86e3e245cd3fe920bdaf9f8a
Navid Shad <navidshad72@gmail.com>
2023-10-04 10:48:22 +0300

    Base project

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      redirect: '/editor'
+    },
+    {
+      path: '/editor',
+      name: 'editor',
+      component: () => import('@/views/Editor.vue')
+    }
+  ]
+})
+
+export default router
