a5d35db96efa8f1dea349228943956faad2adf7c
Navid Shad <navidshad72@gmail.com>
2023-10-05 12:19:52 +0300

    tweak

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    base: '/apps/diagram-designer/',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
