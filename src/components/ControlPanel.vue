5df0db65fe833cd875e070a92a43667dd6fcb483
Navid Shad <navidshad72@gmail.com>
2023-10-17 20:47:00 +0300

    Add splited sectons on property tab

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,33 +1,21 @@
 <template>
   <v-navigation-drawer location="right" permanent :model-value="active" :width="width">
-    <!-- ADDITIONAL PANEL
-     -->
-    <div v-for="(panel, i) in additionalPanels" :key="i">
-      <transition>
-        <slot v-if="activeSlot == panel.slot" :name="panel.slot"></slot>
-      </transition>
-    </div>
+    <section class="flex flex-col h-screen">
+      <v-tabs v-model="tab" fixed-tabs>
+        <v-tab value="properties">Properties</v-tab>
+        <v-tab value="shapes">Shapes</v-tab>
+      </v-tabs>
 
-    <!-- DEFAULT PANEL
-     -->
-    <transition>
-      <div v-if="activeSlot == 'default'">
-        <v-tabs v-model="tab" fixed-tabs>
-          <v-tab value="properties">Properties</v-tab>
-          <v-tab value="shapes">Shapes</v-tab>
-        </v-tabs>
+      <v-window class="flex-1 h-full" v-model="tab">
+        <v-window-item value="shapes" class="h-full">
+          <control-panel-shapes />
+        </v-window-item>
 
-        <v-window v-model="tab">
-          <v-window-item value="shapes">
-            <control-panel-shapes />
-          </v-window-item>
-
-          <v-window-item value="properties">
-            <control-panel-properties />
-          </v-window-item>
-        </v-window>
-      </div>
-    </transition>
+        <v-window-item value="properties" class="h-full">
+          <control-panel-properties />
+        </v-window-item>
+      </v-window>
+    </section>
   </v-navigation-drawer>
 </template>
 
--- src/components/ControlPanelProperties.vue
+++ src/components/ControlPanelProperties.vue
@@ -1,13 +1,33 @@
 <template>
-  <hierarchy />
+  <div class="split h-full">
+    <div id="split-0" class="overflow-y-scroll h-full">
+      <hierarchy />
+    </div>
+    <div id="split-1" class="overflow-y-scroll h-full"></div>
+  </div>
 </template>
 
-<script>
-import Hierarchy from "./Hierarchy.vue";
-export default {
-  components: { Hierarchy },
-};
+<script setup lang="ts">
+import Split from 'split.js'
+import Hierarchy from './Hierarchy.vue'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  Split(['#split-0', '#split-1'], {
+    direction: 'vertical'
+  })
+})
 </script>
 
 <style>
+.gutter {
+  background-color: #eee;
+  background-repeat: no-repeat;
+  background-position: 50%;
+}
+
+.gutter.gutter-vertical {
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAFAQMAAABo7865AAAABlBMVEVHcEzMzMzyAv2sAAAAAXRSTlMAQObYZgAAABBJREFUeF5jOAMEEAIEEFwAn3kMwcB6I2AAAAAASUVORK5CYII=');
+  cursor: row-resize;
+}
 </style>
